81015ebf5e9ae3f8ef8e4a46a325e8884b4eab28
rutujakardile1998 <59498764+rutujakardile1998@users.noreply.github.com>
2020-07-23 22:39:58 +0530

    Add files via upload

---
+++ Day 4/Day 4 assignment letsupgrade.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+# coding: utf-8
+str1="what we think we become,we are python programmer."
+str1.find("we")
+
+# In[1]:
+
+
+str1="what we think we become,we are python programmer."
+str1.find("we")
+str1.rfind("we")
+str1.index("we")
+
+
+# In[5]:
+
+
+str2="HellO"
+str2.islower()
+
+
+# In[6]:
+
+
+str2.isupper()
+
+
+# In[4]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
